95e873616eae506730cefc56f7151f104980d0f3
jesutera <140918552+jesutera@users.noreply.github.com>
2023-11-14 21:20:49 -0500

    la iniciada de sesion y crear cuenta estan en procesos de terminacion

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const showPopuBtn = document.querySelector(".login-btn");
-const hidePopuBtn = document.querySelector(".form-popup .close-btn");
+const hidePopuBtn = document.querySelector(".form-popup");
+const formPopup = document.querySelector(".form-popup .close-btn");
+const loginSignuplink = document.querySelectorAll(".form-box .bottom-link a");
 
 // Show form popup
 showPopuBtn.addEventListener("click", () => {
@@ -8,3 +10,10 @@ showPopuBtn.addEventListener("click", () => {
 
 // Hide form popup
 hidePopuBtn.addEventListener("click", ()=> showPopuBtn.click());
+
+loginSignuplink.forEach(link => {
+    link.addEventListener("click", (e) => {
+         e.preventDefault();
+        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove'] ("show-signup");
+    });
+});
