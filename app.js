2c6ea182b6df15eba7775c328d382d46dfaf0d9f
jesutera <140918552+jesutera@users.noreply.github.com>
2023-11-14 20:33:48 -0500

    Ya se oprime iniciar sesión y se sale por medio de la x

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const showPopuBtn = document.querySelector(".login-btn")
+const showPopuBtn = document.querySelector(".login-btn");
+const hidePopuBtn = document.querySelector(".form-popup .close-btn");
 
+// Show form popup
 showPopuBtn.addEventListener("click", () => {
     document.body.classList.toggle("show-popup");
 });
+
+// Hide form popup
+hidePopuBtn.addEventListener("click", ()=> showPopuBtn.click());
