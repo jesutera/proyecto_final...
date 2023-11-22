1880c32478a372c054262f73b598ceca653876ef
jesutera <140918552+jesutera@users.noreply.github.com>
2023-11-22 14:59:30 -0500

    el diseño responsive se esta creando

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const showPopuBtn = document.querySelector(".login-btn");
-const hidePopuBtn = document.querySelector(".form-popup");
-const formPopup = document.querySelector(".form-popup .close-btn");
-const loginSignuplink = document.querySelectorAll(".form-box .bottom-link a");
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
 
 // Show form popup
-showPopuBtn.addEventListener("click", () => {
+showPopupBtn.addEventListener("click", () => {
     document.body.classList.toggle("show-popup");
 });
 
 // Hide form popup
-hidePopuBtn.addEventListener("click", ()=> showPopuBtn.click());
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
 
-loginSignuplink.forEach(link => {
+loginSignupLink.forEach(link => {
     link.addEventListener("click", (e) => {
          e.preventDefault();
-        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove'] ("show-signup");
+        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
     });
 });
