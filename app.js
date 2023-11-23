cebf121c5d316fed65c6a134e9ce3190edbd3587
jesutera <140918552+jesutera@users.noreply.github.com>
2023-11-22 21:20:26 -0500

    Ya estoy cansado

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const navbarMenu = document.querySelector(".navbar .links")
+const menuBtn = document.querySelector(".menu-btn");
 const showPopupBtn = document.querySelector(".login-btn");
 const formPopup = document.querySelector(".form-popup");
 const hidePopupBtn = document.querySelector(".form-popup .close-btn");
 const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
+
+menuBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
+});
+
+
 
 // Show form popup
 showPopupBtn.addEventListener("click", () => {
